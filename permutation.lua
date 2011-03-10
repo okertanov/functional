8681c41467af744bdd534af8354b14f1f2d970b1
okertanov <okertanov@gmail.com>
2011-03-10 23:13:42 +0200

    luafication

--- permutation.lua
+++ permutation.lua
@@ -1,4 +1,4 @@
-function permgen (a, n)
+function perm_generate(a, n)
   if n == 0 then
     coroutine.yield(a)
   else
@@ -8,7 +8,7 @@ function permgen (a, n)
       a[n], a[i] = a[i], a[n]
 
       -- generate all permutations of the other elements
-      permgen(a, n - 1)
+      perm_generate(a, n - 1)
 
       -- restore i-th element
       a[n], a[i] = a[i], a[n]
@@ -18,21 +18,22 @@ function permgen (a, n)
 end
 
 
-function perm (a)
+function permutation(a)
   local n = table.getn(a)
-  return coroutine.wrap(function () permgen(a, n) end)
+  return coroutine.wrap(function () perm_generate(a, n) end)
 end
 
 
-function printResult (a)
+function print_result (a)
   for i,v in ipairs(a) do
     io.write(v, " ")
   end
   io.write("\n")
 end
 
+itable = {"0","1","2"};
 
-for p in perm{"1", "2", "3"} do
-  printResult(p)
+for p in permutation(itable) do
+  print_result(p)
 end
 
