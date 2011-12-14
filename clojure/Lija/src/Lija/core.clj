06a99713d67c76367f589dbd94a418da6b0a43bc
okertanov <okertanov@gmail.com>
2011-12-14 17:56:32 +0200

    ants by Hickey, and Lija extended to UI

--- clojure/Lija/src/Lija/core.clj
+++ clojure/Lija/src/Lija/core.clj
@@ -1,5 +1,26 @@
 (ns Lija.core)
 
-(defn -main [& args]
-    (println "Welcome to Lija! Args are:" args))
+(import
+ '(java.awt Color Graphics Dimension GridLayout)
+ '(javax.swing JPanel JFrame JLabel JButton))
 
+(defn render [g] (do))
+
+(def width 320)
+(def height 240)
+
+(def panel (doto (new JPanel)
+            (.setLayout (new GridLayout 0 2 2 2))
+            (.add (new JButton ""))
+            (.add (new JButton ""))
+            (.add (new JButton ""))
+            (.add (new JButton ""))))
+
+(defn -main [& args]
+  (def frame (doto (new JFrame)
+              (.setPreferredSize (new Dimension width height))
+              (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+              (.add panel)
+              .pack
+              .show))
+  (println "Welcome to Lija! Args are:" args))
