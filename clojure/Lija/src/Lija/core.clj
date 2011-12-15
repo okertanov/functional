2739bdf779fae0743e218c9f973b4b668711b3c1
okertanov <okertanov@gmail.com>
2011-12-15 11:59:49 +0200

    redo the code

--- clojure/Lija/src/Lija/core.clj
+++ clojure/Lija/src/Lija/core.clj
@@ -1,26 +1,37 @@
 (ns Lija.core)
 
 (import
- '(java.awt Color Graphics Dimension GridLayout)
- '(javax.swing JPanel JFrame JLabel JButton))
+  '(java.awt Color Graphics Dimension GridLayout)
+  '(javax.swing JPanel JFrame JLabel JButton))
 
-(defn render [g] (do))
+(defprotocol Dimension-protocol
+  (create-dimension [this]
+    "Creates new Dimension class"))
 
-(def width 320)
-(def height 240)
+(defrecord size [w h]
+  Dimension-protocol
+    (create-dimension [this] (new Dimension w h)))
 
-(def panel (doto (new JPanel)
-            (.setLayout (new GridLayout 0 2 2 2))
-            (.add (new JButton ""))
-            (.add (new JButton ""))
-            (.add (new JButton ""))
-            (.add (new JButton ""))))
+(def wsize
+  (size. 320 240))
+
+(defn layout []
+  (new GridLayout 0 2 2 2))
+
+(def panel
+  (doto (new JPanel)
+    (.setLayout (layout))
+    (.add (new JButton ""))
+    (.add (new JButton ""))
+    (.add (new JButton ""))
+    (.add (new JButton ""))))
 
 (defn -main [& args]
-  (def frame (doto (new JFrame)
-              (.setPreferredSize (new Dimension width height))
-              (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-              (.add panel)
-              .pack
-              .show))
+  (def frame
+    (doto (new JFrame)
+    (.setPreferredSize (.create-dimension wsize))
+    (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+    (.add panel)
+    .pack
+    .show))
   (println "Welcome to Lija! Args are:" args))
