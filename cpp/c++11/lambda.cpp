4af88edcb69afa43dce14b503a3bc1d14fe3884d
okertanov <okertanov@gmail.com>
2012-02-08 10:22:10 +0200

    lambda code cleanup

--- cpp/c++11/lambda.cpp
+++ cpp/c++11/lambda.cpp
@@ -37,19 +37,6 @@ auto hof_val(int a) -> std::function<int (int)>
     };
 }
 
-auto hof_ref(int a) -> std::function<int (int)>
-{
-    std::cout << "Inside " << __PRETTY_FUNCTION__ << ": a = " << a << std::endl;
-
-    return [&](int b) -> int
-    {
-        std::cout << "Inside " << __PRETTY_FUNCTION__ << ": "<<
-                     "a = " << a << " & " <<
-                     "b = " << b << std::endl;
-        return (a * b);
-    };
-}
-
 template <typename T>
 auto pow2(const T x) -> decltype(x)
 {
@@ -66,11 +53,8 @@ auto main(int argc, char** argv) -> int
     std::for_each(tdata.rbegin(), tdata.rend(), hof_make_iter());
 
     // 3. higher-order function
-    auto ret1 = (hof_val(hof_val(1)(2))(3));
-    auto ret2 = (hof_ref(hof_ref(1)(2))(3));
-
-    std::cout << "1st lambda returned = " << ret1 << std::endl;
-    std::cout << "2nd lambda returned = " << ret2 << std::endl;
+    auto ret = (hof_val(hof_val(1)(2))(3));
+    std::cout << "lambda returned = " << ret << std::endl;
 
     // 4. decltype
     auto px = pow2(std::complex<double>{2, 1});
