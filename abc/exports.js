b77df8acf86260b9ebcae2e02445f8b7276ac59a
okertanov <okertanov@gmail.com>
2012-03-31 12:01:14 +0300

    exports.js added

---
+++ abc/exports.js
@@ -0,0 +1,24 @@
+;(function(exports) {
+    'use strict';
+
+    // First you define your class
+    function SomeClass() {
+        // code...
+    }
+
+    SomeClass.prototype.foo = function() {
+        // code...
+    };
+
+    // And then you expose it
+    exports.SomeClass = SomeClass;
+}(this)); // <-- this = the global object is passed as exports
+
+
+// This is only required for server side environments such as node.js
+// In the browser you would use a script tag to load it
+var SomeClass = require('someclass').SomeClass;
+
+// Then you can call the class like this
+var foo = new SomeClass();
+
