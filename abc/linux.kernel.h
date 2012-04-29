315802104ec635667c6fe41078e425681903afe3
okertanov <okertanov@gmail.com>
2012-04-29 16:37:26 +0300

    tricky static assert added

---
+++ abc/linux.kernel.h
@@ -0,0 +1,10 @@
+/* Force a compilation error if condition is true, but also produce a
+   result (of value 0 and type size_t), so the expression can be used
+   e.g. in a structure initializer (or where-ever else comma expressions
+   aren't permitted). */
+#define BUILD_BUG_ON_ZERO(e) (sizeof(struct { int:-!!(e); }))
+#define BUILD_BUG_ON_NULL(e) ((void *)sizeof(struct { int:-!!(e); }))
+
+/**
+    @see http://stackoverflow.com/questions/9229601/what-is-it-in-c-code
+*/
