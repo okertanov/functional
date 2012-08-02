ac403d2d7df0fb08d9b804730be48584658c8fd9
okertanov <okertanov@gmail.com>
2012-08-02 22:52:17 +0300

    add1 make-up

--- cpp/c++11/add1.cpp
+++ cpp/c++11/add1.cpp
@@ -28,9 +28,9 @@ return: arr with proper values incremented
 
     Examples:
 
-# add1 [1,4,1,5,1], 1, 0  -> [2,4,2,5,2]
-# add1 [1,4,1,5,1], 1, 2  -> [2,4,2,5,1]
-# add1 [1,4,1,5,1], 1, -2 -> [1,4,2,5,2]
+add1 [1,4,1,5,1], 1, 0  -> [2,4,2,5,2]
+add1 [1,4,1,5,1], 1, 2  -> [2,4,2,5,1]
+add1 [1,4,1,5,1], 1, -2 -> [1,4,2,5,2]
 
     To build:
 
@@ -39,10 +39,12 @@ g++ -std=c++0x -o add1 add1.cpp
 */
 
 #include <cmath>
+#include <string>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
 #include <functional>
+#include <tuple>
 #include <deque>
 
 namespace Exec
@@ -52,17 +54,24 @@ namespace Exec
     @typedef Exec::IntegerArray
     @brief
 */
-typedef std::deque<int>    IntegerArray;
+typedef std::deque<int>             IntegerArray;
+
+/**
+    @typedef Exec::Add1Result
+    @brief
+*/
+typedef std::typle<size_t, size_t>  Add1Result;
 
 /**
     @fn Exec::add1_impl()
     @brief
 */
 template <typename T, typename InputIterator>
-bool add1_impl(const InputIterator& begin,  const InputIterator& end, const typename T::value_type& value, const signed long n)
+Add1Result add1_impl(const InputIterator& begin,  const InputIterator& end, const typename T::value_type& value, const signed long n)
 {
     auto found = begin;
-    auto count = ( n ? std::abs(n) : std::distance(begin, end) );
+    size_t count = ( n ? std::abs(n) : std::distance(begin, end) ),
+           matched = 0L;
 
     do
     {
@@ -80,7 +89,7 @@ bool add1_impl(const InputIterator& begin,  const InputIterator& end, const type
     }
     while ( --count > 0 && found != end );
 
-    return true;
+    return std::make_typle(count, matched);
 }
 
 /**
@@ -90,14 +99,9 @@ bool add1_impl(const InputIterator& begin,  const InputIterator& end, const type
 template <typename T>
 T add1(T& array, const typename T::value_type& value, const signed long n)
 {
-    if ( n < 0 )
-    {
-        add1_impl<T>(array.rbegin(), array.rend(), value, n);
-    }
-    else
-    {
+    Add1Result ares = n < 0 ?
+        add1_impl<T>(array.rbegin(), array.rend(), value, n) :
         add1_impl<T>(array.begin(), array.end(), value, n);
-    }
 
     return array;
 }
@@ -120,20 +124,31 @@ std::ostream& operator<< (std::ostream& os, const Exec::IntegerArray& array)
 */
 auto main(int argc, char* argv[]) -> int
 {
-    const Exec::IntegerArray reference_vec = {1, 4, 1, 5, 1};
+    try
+    {
+        const Exec::IntegerArray reference_vec = {1, 4, 1, 5, 1};
 
-    Exec::IntegerArray test_vec1(reference_vec),
-                       test_vec2(reference_vec),
-                       test_vec3(reference_vec);
+        Exec::IntegerArray test_vec1(reference_vec),
+                           test_vec2(reference_vec),
+                           test_vec3(reference_vec);
 
-    std::cout << test_vec1 << std::endl
-              << test_vec2 << std::endl
-              << test_vec3 << std::endl;
+        std::cout << test_vec1 << std::endl
+                  << test_vec2 << std::endl
+                  << test_vec3 << std::endl;
 
-    std::cout << "~~~~~~~~~" << std::endl;
+        std::cout << std::string(reference_vec.size() * 2, '~') << std::endl;
 
-    std::cout << Exec::add1(test_vec1, 1, 0)  << std::endl
-              << Exec::add1(test_vec2, 1, 2)  << std::endl
-              << Exec::add1(test_vec3, 1, -2) << std::endl;
+        std::cout << Exec::add1(test_vec1, 1, 0)  << std::endl
+                  << Exec::add1(test_vec2, 1, 2)  << std::endl
+                  << Exec::add1(test_vec3, 1, -2) << std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    catch(...)
+    {
+        std::cerr << "Error: " << "Unknown exception." << std::endl;
+    }
 }
 
