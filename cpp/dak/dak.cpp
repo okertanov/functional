ebab86610802dd5c1cc4deecac1a35c98ffd7407
Oleg Kertanov <okertanov@gmail.com>
2017-01-05 16:42:12 +0200

    semi-simd impl done

--- cpp/dak/dak.cpp
+++ cpp/dak/dak.cpp
@@ -1,44 +1,90 @@
 //
 // See http://maxim.livejournal.com/494874.html
 //
-// c++ -O3 -Wall -Wextra -std=c++11 -o dak dak.cpp
-//      -O3 -msse3 -ffast-math.
-//      -ftree-vectorizer-verbose=10
-//
 
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 #include <iostream>
 #include <chrono>
+#include <emmintrin.h>
 
-int main() {
+int64_t plain_sum(const std::vector<int64_t>& vec, int step) {
+    const size_t size = vec.capacity();
     int64_t sum = 0;
 
-   auto start = std::chrono::high_resolution_clock::now();
-
-    for (int e = 0; e < 20; e++) {
-        sum = 0;
-
-        std::vector<int64_t> x;
-        x.reserve(1000000);
-        for (int i = 0; i < 1000000; i++) {
-            x[i] = i;
-        }
-
-        std::vector<int64_t> y;
-        y.reserve(1000000 - 1);
-        for (int i = 0; i < 1000000-1; i++) {
-            y[i] = x[i] + x[i+1];
-        }
-
-        for (int i = 0; i < 1000000; i += 100) {
-            sum += y[i];
-        }
+    std::vector<int64_t> sums;
+    sums.reserve(size - 1);
+    for (size_t i = 0; i < size - 1; i++) {
+        sums[i] = vec[i] + vec[i + 1];
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    for (size_t i = 0; i < size; i += step) {
+        sum += sums[i];
+    }
 
-    std::cout << sum << std::endl;
-    std::cout << "time: " <<  diff << " ms" << std::endl;
+    return sum;
+}
+
+int64_t simd_sum(const std::vector<int64_t>& vec, int step) {
+    const size_t size = vec.capacity();
+    int64_t sum = 0;
+
+    std::vector<int64_t> sums __attribute__((aligned(16)));
+    sums.reserve(size);
+
+    std::vector<int64_t>& avec = const_cast<std::vector<int64_t>&>(vec);
+
+    for (size_t i = 0; i < size - 1; i += 2) {
+        __m128i av128_1 = _mm_loadu_si128(reinterpret_cast<__m128i*>(&avec[i]));
+        __m128i av128_2 = _mm_loadu_si128(reinterpret_cast<__m128i*>(&avec[i + 1]));
+        __m128i r = _mm_add_epi64(av128_1, av128_2);
+        __m128i* sums_simd = reinterpret_cast<__m128i*>(&sums[i]);
+        _mm_store_si128(sums_simd, r);
+    }
+
+    for (size_t i = 0; i < size; i += step) {
+        sum += sums[i];
+    }
+
+    return sum;
+}
+
+int main(int argc __attribute__((unused)), char** argv __attribute__((unused))) {
+    const int step = 100;
+    const int runs = 20;
+    const size_t size = 1000000;
+
+    int64_t sum1 = 0ull, sum2 = 0ull;
+
+    // Prepare data
+    std::vector<int64_t> vec1;
+    vec1.reserve(size);
+    for (size_t i = 0; i < size; i++) {
+        vec1[i] = i;
+    }
+
+    // SIMD Sum
+    auto start1 = std::chrono::high_resolution_clock::now();
+    for (int e = 0; e < runs; e++) {
+        sum1 = simd_sum(vec1, step);
+    }
+    auto end1 = std::chrono::high_resolution_clock::now();
+    auto diff1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count();
+
+    // Reset Caches?
+
+    // Plain Sum
+    auto start2 = std::chrono::high_resolution_clock::now();
+    for (int e = 0; e < runs; e++) {
+        sum2 = plain_sum(vec1, step);
+    }
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto diff2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2).count();
+
+    // Dump
+    std::cout << sum1 << " -- time for simd:  " << diff1 << " ms." << std::endl;
+    std::cout << sum2 << " -- time for plain: " << diff2 << " ms." << std::endl;
+
+    return EXIT_SUCCESS;
 }
